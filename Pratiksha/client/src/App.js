36f9adad78b3e37ee6edd2fbb81beca9c1605010
parth gaikwad <parthgaikwad752@gmail.com>
2023-05-27 13:46:49 +0530

    Commit

--- Pratiksha/client/src/App.js
+++ Pratiksha/client/src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import "./App.css";
 import Sidebar from "./Components/Sidebar";
 import Navbar from "./Components/Navbar";
+import AppBar from "./Components/AppBar";
+import New from "./Components/New"
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Sidebar />
+      <AppBar/>
+      <New/>
     </div>
   );
 }
